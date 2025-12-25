ab60d7886e483f7a7b0cc5e55bcceebf9610326e
Queenofclouds-code <meghaj9096@gmail.com>
2025-12-25 10:23:34 +0530

    Raster Size

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const puneRaster = L.tileLayer(
   "/portal/static/Raster_Tiles/Pune/{z}/{x}/{y}.png",
   {
     minZoom: 11,
-    maxZoom: 18,
+    maxZoom: 17,
     tms: true,        // ✅ Leaflet handles Y flip
     opacity: 1
   }
