4e66ffce27ab3c96669484054d8dc3abb4449987
Queenofclouds-code <meghaj9096@gmail.com>
2025-12-25 11:43:42 +0530

    Raster Size

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ const puneRaster = L.tileLayer(
     minZoom: 11,
     maxZoom: 22,
     tms: true,        // ✅ Leaflet handles Y flip
-    opacity: 0.8,
-    zIndex: 2
+    opacity: 1.5,
+    zIndex: 1
   }
 );
 
