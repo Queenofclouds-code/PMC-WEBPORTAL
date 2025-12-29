b3ecc2b0505fd868ef2696b384e3d606a74ba9c5
Queenofclouds-code <meghaj9096@gmail.com>
2025-12-29 15:41:40 +0530

    New Rasters Added

--- main.js
+++ main.js
@@ -103,8 +103,8 @@ const rasterRegistry = [
     name: "Narmada Part2",
     layer: narmadaPart2Raster,
     bounds: [ 
-      [23.1165, 79.8805], 
-      [23.1220, 79.8830]   
+      [23.1165, 79.8755], 
+      [23.1220, 79.8780]   
     ]
   },
   {
